66ba11891d5de4473d7ad4f68a4b1abfc3e2b536
josearrais <josearrais@users.noreply.github.com>
2025-09-23 01:28:58 +0100

    feat(config): add navigation data

---
+++ src/config/navigation.js
@@ -0,0 +1,11 @@
+export const homePageNav = [
+  { name: 'About', link: '/' },
+  { name: 'Explore', link: '/' },
+  { name: 'Docs', link: '/' }
+]
+
+export const footerLinks = [
+  { name: 'GitHub', link: 'https://github.com/josearrais/agio' },
+  { name: 'Privacy Policy', link: '/' },
+  { name: 'Terms of Service', link: '/' }
+]
