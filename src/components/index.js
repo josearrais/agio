c250413633af192a7c9232d247f3940c136e0f30
josearrais <josearrais@users.noreply.github.com>
2025-09-23 01:25:57 +0100

    feat(pages): add homepage layout and routing exports

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Header } from './Header/index';
+export { default as Hero } from './Hero/index';
+export { default as Footer } from './Footer/index';
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import { Header, Hero, Footer } from '@/components';
+
+const HomePage = () => {
+  return (
+    <div id='bg'>
+      <Header />
+      <Hero />
+      <Footer />
+    </div>
+  )
+}
+
+export default HomePage;
