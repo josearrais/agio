9cc3637f183b0e01ae534be55901169e4e00096d
josearrais <josearrais@users.noreply.github.com>
2025-10-03 00:51:40 +0100

    feat: add routes and exports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { HomePage } from './pages';
+import { HomePage, Swap, NotFoundPage } from './pages';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='swap' element={<Swap />} />
+        <Route path='*' element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,3 @@
 export { default as HomePage } from './HomePage';
+export { default as Swap } from './Swap';
+export { default as NotFoundPage } from './NotFoundPage';
